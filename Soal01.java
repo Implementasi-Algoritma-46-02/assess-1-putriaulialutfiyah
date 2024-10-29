2dadf012af4fa7d993c511b083c6e3ca6bac5156
putriaulialutfiyah <putriaulialuthfiyah4140@gmail.com>
2024-10-29 16:06:50 +0700

    tes

--- Soal01.java
+++ Soal01.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Soal01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner s = new Scanner(System.in);
+
+		
 	}
 }
--- Soal03.java
+++ Soal03.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Soal03 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner s = new Scanner(System.in);
+
+		int H = s.nextInt();
+		int M = s.nextInt();
+		int V = s.nextInt();
+
+		int jumlah = M + V;
+
+		System.out.println(H, jumlah);
 	}
 }
